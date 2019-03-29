fae3f6213c4681b458ac0cdc3388e1ff3bda9f4d
Jack <jackjin.com@hotmail.com>
2019-03-29 00:41:19 -0400

    ViewModels

--- AspNetCoreWebDemo/AspNetCoreWebDemo_MVC/Controllers/HomeController.cs
+++ AspNetCoreWebDemo/AspNetCoreWebDemo_MVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AspNetCoreWebDemo_MVC.Models;
+using AspNetCoreWebDemo_MVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -55,6 +56,54 @@ namespace AspNetCoreWebDemo_MVC.Controllers
      * services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
      * 
      *************************************************************************/
+    //public class HomeController : Controller
+    //{
+    //    private IEmployeeRepository _EmployeeRepository;
+
+    //    public HomeController(IEmployeeRepository employeeRepository)
+    //    {
+    //        _EmployeeRepository = employeeRepository;
+    //    }
+
+    //    public ObjectResult Index()
+    //    {
+    //        Employee emp = _EmployeeRepository.GetEmployee(2);
+
+    //        return new ObjectResult(emp);
+    //    }
+    //}
+
+    /* Demo #5 ***************************************************************
+     * ViewData
+     * ViewBag
+     * Strongly Type
+     * 
+     *************************************************************************/
+    //public class HomeController : Controller
+    //{
+    //    private IEmployeeRepository _EmployeeRepository;
+
+    //    public HomeController(IEmployeeRepository employeeRepository)
+    //    {
+    //        _EmployeeRepository = employeeRepository;
+    //    }
+
+    //    public ViewResult Details()
+    //    {
+    //        Employee emp = _EmployeeRepository.GetEmployee(2);
+
+    //        ViewData["Employee"] = emp;
+
+    //        ViewBag.PageTitle = "Employee Details";
+    //        ViewBag.Employee = emp;
+
+    //        return View(emp);
+    //    }
+    //}
+
+    /* Demo #5 ***************************************************************
+     * ViewModel
+     *************************************************************************/
     public class HomeController : Controller
     {
         private IEmployeeRepository _EmployeeRepository;
@@ -64,13 +113,16 @@ namespace AspNetCoreWebDemo_MVC.Controllers
             _EmployeeRepository = employeeRepository;
         }
 
-        public ObjectResult Index()
+        public ViewResult Details1()
         {
-            Employee emp = _EmployeeRepository.GetEmployee(2);
+            HomeDetails1ViewModel homeDetails1ViewModel = new HomeDetails1ViewModel()
+            {
+                PageTitle = "Employee Details",
+                Employee = _EmployeeRepository.GetEmployee(2)
+            };
 
-            return new ObjectResult(emp);
+            return View(homeDetails1ViewModel);
         }
     }
-
 
 }
---
+++ AspNetCoreWebDemo/AspNetCoreWebDemo_MVC/ViewModels/HomeDetails1ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AspNetCoreWebDemo_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebDemo_MVC.ViewModels
+{
+    public class HomeDetails1ViewModel
+    {
+        public string PageTitle { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
